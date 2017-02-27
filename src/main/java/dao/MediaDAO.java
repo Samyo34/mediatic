f2228abd72fee2d282665dd5b656b9c89da38447
Your Name <you@example.com>
2017-02-27 15:35:23 +0100

    Commit media

--- src/main/java/dao/MediaDAO.java
+++ src/main/java/dao/MediaDAO.java
@@ -1,13 +1,17 @@
 package dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import globale.DatabaseHelper;
+import model.Adherent;
 import model.Media;
 
 public class MediaDAO extends DAO{
 	
-	public static Media getAdherentByID(Long id){
+	public static Media getMediaByID(Long id){
 		EntityManager em = DatabaseHelper.createEntityManager();
 		DatabaseHelper.beginTx(em);
 		Media md = em.find(Media.class,id);
@@ -15,4 +19,12 @@ public class MediaDAO extends DAO{
 		return md;
 	}
 
+	public static Media findOneWithAdherent(Long id){
+		EntityManager em = DatabaseHelper.createEntityManager();
+		DatabaseHelper.beginTx(em);
+		Media md = em.find(Media.class,id);
+		DatabaseHelper.commitTxAndClose(em);
+		return md;
+	}
+	
 }
--- src/main/java/model/Media.java
+++ src/main/java/model/Media.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -7,6 +9,7 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Media {
@@ -24,4 +27,8 @@ public class Media {
 	
 	@Column(nullable = false)
 	private String auteur;
+	
+	@Column
+	@OneToMany (mappedBy = "media")
+	private List<Emprunt> emprunts;
 }
