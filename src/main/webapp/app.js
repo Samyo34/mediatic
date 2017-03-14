5caffcdee4055cc9e9e66533a6e0ce6972864f47
Your Name <you@example.com>
2017-03-14 14:57:42 +0100

    Resolved modules

--- src/main/webapp/app.js
+++
@@ -1,7 +0,0 @@
-var mediaticApp = angular.module('mediaticApp', ['ngRoute' , 'mediaticApp.adherent', 'mediaticApp.media']);
-
-mediaticApp.config(['$routeProvider',
-	function($routeProvider) {
-	  $routeProvider.otherwise({ redirectTo: '/medias'});
-	}
-]);
