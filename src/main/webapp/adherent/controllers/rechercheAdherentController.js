33c8f767cbb24269c2430fa385d03a222d82ccd3
Your Name <you@example.com>
2017-03-16 15:25:34 +0100

    Tri dynamique Recherches

--- src/main/webapp/adherent/controllers/rechercheAdherentController.js
+++ src/main/webapp/adherent/controllers/rechercheAdherentController.js
@@ -1,117 +1,173 @@
-(function(){
+(function() {
 	'use strict';
-	
-	angular.module('mediaticApp.adherent')
-	.config(function($routeProvider){
-		$routeProvider.when('/adherents',{
-			templateUrl:'/adherent/templates/rechercheAdherent.html',
-			controller:'RechercheAdherentCtrl'
-		})
-	})
-	.controller('RechercheAdherentCtrl',function($scope, $routeParams, $rootScope, ServiceUrl){
-		$rootScope.title = "Recherche Adhérents";
 
-		$scope.currentDate = new Date();
-		$scope.sortedId = false;
-		
-		$scope.sortId = function() {
-			$scope.sortedId = !$scope.sortedId;
-			var params = {tri: "id"};
-			if($scope.sortedId)
-			{
-				params.tri = "-id";
-			}
-			ServiceUrl.getAdherentsByParams(params).then(function(adherents){
-				$scope.adherents = adherents;
-			});
-		}
-		
-		$scope.sortedPrenom = false;
-		
-		$scope.sortPrenom = function() {
-			$scope.sortedPrenom = !$scope.sortedPrenom;
-			var params = {tri: "prenom"};
-			if($scope.sortedPrenom)
-			{
-				params.tri = "-prenom";
-			}
-			ServiceUrl.getAdherentsByParams(params).then(function(adherents){
-				$scope.adherents = adherents;
-			});
-		}
-		
-		$scope.sortedNom = false;
-		
-		$scope.sortNom = function() {
-			$scope.sortedNom = !$scope.sortedNom;
-			var params = {tri: "nom"};
-			if($scope.sortedNom)
-			{
-				params.tri = "-nom";
-			}
-			ServiceUrl.getAdherentsByParams(params).then(function(adherents){
-				$scope.adherents = adherents;
-			});
-		}
-		
-		$scope.sortedEmail = false;
-		
-		$scope.sortEmail = function() {
-			$scope.sortedEmail = !$scope.sortedEmail;
-			var params = {tri: "email"};
-			if($scope.sortedEmail)
-			{
-				params.tri = "-email";
-			}
-			ServiceUrl.getAdherentsByParams(params).then(function(adherents){
-				$scope.adherents = adherents;
-			});
-		}
-		
-		$scope.sizeArrayAd = 10;
-		$scope.sizeMaxAd = false;
-		
-		$scope.adherents = [];
-		ServiceUrl.getAdherents().then(function(adherents){
-			$scope.adherents = adherents;
-		});
-		
-		$scope.startSearch = function() {
-			var params = {};
-			if($scope.search.id != undefined)
-			{
-				params.id = $scope.search.id;
-			}
-			if($scope.search.nom != undefined)
-			{
-				params.nom = $scope.search.nom;
-			}
-			if($scope.search.prenom != undefined)
-			{
-				params.prenom = $scope.search.prenom;
-			}
-			if($scope.search.email != undefined)
-			{
-				params.email = $scope.search.email;
-			}
-			if($scope.search.nomOuPrenom != undefined)
-			{
-				params.nomOuPrenom = $scope.search.nomOuPrenom;
-			}
-			ServiceUrl.getAdherentsByParams(params).then(function(adherents){
-				$scope.adherents = adherents;
-			});
-		}
-		
-		$scope.addSize = function(){
-			$scope.sizeArrayAd += 20;
-			if($scope.sizeArrayAd >= $scope.adherents.length){
-				$scope.sizeMaxAd = true;
-			}
-		}
-		
-		
-		
-	})
-	
+	angular.module('mediaticApp.adherent').config(function($routeProvider) {
+		$routeProvider.when('/adherents', {
+			templateUrl : '/adherent/templates/rechercheAdherent.html',
+			controller : 'RechercheAdherentCtrl'
+		})
+	}).controller(
+			'RechercheAdherentCtrl',
+			function($scope, $routeParams, $rootScope, ServiceUrl) {
+				$rootScope.title = "Recherche Adhérents";
+
+				$scope.currentDate = new Date();
+
+				$scope.resetSort = function(sortValue) {
+					if (sortValue != $scope.sortedIdAsc)
+						$scope.sortedIdAsc = false;
+					if (sortValue != $scope.sortedIdDesc)
+						$scope.sortedIdDesc = false;
+					if (sortValue != $scope.sortedPrenomAsc)
+						$scope.sortedPrenomAsc = false;
+					if (sortValue != $scope.sortedPrenomDesc)
+						$scope.sortedPrenomDesc = false;
+					if (sortValue != $scope.sortedNomAsc)
+						$scope.sortedNomAsc = false;
+					if (sortValue != $scope.sortedNomDesc)
+						$scope.sortedNomDesc = false;
+					if (sortValue != $scope.sortedEmailAsc)
+						$scope.sortedEmailAsc = false;
+					if (sortValue != $scope.sortedEmailDesc)
+						$scope.sortedEmailDesc = false;
+					if (sortValue != $scope.sortedNaissanceAsc)
+						$scope.sortedNaissanceAsc = false;
+					if (sortValue != $scope.sortedNaissanceDesc)
+						$scope.sortedNaissanceDesc = false;
+					if (sortValue != $scope.sortedCotisationAsc)
+						$scope.sortedCotisationAsc = false;
+					if (sortValue != $scope.sortedCotisationDesc)
+						$scope.sortedCotisationDesc = false;
+					if (sortValue != $scope.sortedPossessionsAsc)
+						$scope.sortedPossessionsAsc = false;
+					if (sortValue != $scope.sortedPossessionsDesc)
+						$scope.sortedPossessionsDesc = false;
+				}
+
+				$scope.resetSort();
+
+				$scope.sortId = function() {
+					if ($scope.sortedIdAsc) {
+						$scope.resetSort($scope.sortedIdAsc);
+						$scope.sortedIdAsc = false;
+						$scope.sortedIdDesc = true;
+					} else {
+						$scope.resetSort($scope.sortedIdDesc);
+						$scope.sortedIdDesc = false;
+						$scope.sortedIdAsc = true;
+					}
+				}
+
+				$scope.sortPrenom = function() {
+					if ($scope.sortedPrenomAsc) {
+						$scope.resetSort($scope.sortedPrenomAsc);
+						$scope.sortedPrenomAsc = false;
+						$scope.sortedPrenomDesc = true;
+					} else {
+						$scope.resetSort($scope.sortedPrenomDesc);
+						$scope.sortedPrenomDesc = false;
+						$scope.sortedPrenomAsc = true;
+					}
+				}
+
+				$scope.sortNom = function() {
+					if ($scope.sortedNomAsc) {
+						$scope.resetSort($scope.sortedNomAsc);
+						$scope.sortedNomAsc = false;
+						$scope.sortedNomDesc = true;
+					} else {
+						$scope.resetSort($scope.sortedNomDesc);
+						$scope.sortedNomDesc = false;
+						$scope.sortedNomAsc = true;
+					}
+				}
+
+				$scope.sortEmail = function() {
+					if ($scope.sortedEmailAsc) {
+						$scope.resetSort($scope.sortedEmailAsc);
+						$scope.sortedEmailAsc = false;
+						$scope.sortedEmailDesc = true;
+					} else {
+						$scope.resetSort($scope.sortedEmailDesc);
+						$scope.sortedEmailDesc = false;
+						$scope.sortedEmailAsc = true;
+					}
+				}
+
+				$scope.sortNaissance = function() {
+					if ($scope.sortedNaissanceAsc) {
+						$scope.resetSort($scope.sortedNaissanceAsc);
+						$scope.sortedNaissanceAsc = false;
+						$scope.sortedNaissanceDesc = true;
+					} else {
+						$scope.resetSort($scope.sortedNaissanceDesc);
+						$scope.sortedNaissanceDesc = false;
+						$scope.sortedNaissanceAsc = true;
+					}
+				}
+				
+				$scope.sortCotisation = function() {
+					if ($scope.sortedCotisationAsc) {
+						$scope.resetSort($scope.sortedCotisationAsc);
+						$scope.sortedCotisationAsc = false;
+						$scope.sortedCotisationDesc = true;
+					} else {
+						$scope.resetSort($scope.sortedCotisationDesc);
+						$scope.sortedCotisationDesc = false;
+						$scope.sortedCotisationAsc = true;
+					}
+				}
+				
+				$scope.sortPossessions = function() {
+					if ($scope.sortedPossessionsAsc) {
+						$scope.resetSort($scope.sortedPossessionsAsc);
+						$scope.sortedPossessionsAsc = false;
+						$scope.sortedPossessionsDesc = true;
+					} else {
+						$scope.resetSort($scope.sortedPossessionsDesc);
+						$scope.sortedPossessionsDesc = false;
+						$scope.sortedPossessionsAsc = true;
+					}
+				}
+
+				$scope.sizeArrayAd = 10;
+				$scope.sizeMaxAd = false;
+
+				$scope.adherents = [];
+				ServiceUrl.getAdherents().then(function(adherents) {
+					$scope.adherents = adherents;
+				});
+
+				$scope.startSearch = function() {
+					var params = {};
+					if ($scope.search.id != undefined) {
+						params.id = $scope.search.id;
+					}
+					if ($scope.search.nom != undefined) {
+						params.nom = $scope.search.nom;
+					}
+					if ($scope.search.prenom != undefined) {
+						params.prenom = $scope.search.prenom;
+					}
+					if ($scope.search.email != undefined) {
+						params.email = $scope.search.email;
+					}
+					if ($scope.search.nomOuPrenom != undefined) {
+						params.nomOuPrenom = $scope.search.nomOuPrenom;
+					}
+					ServiceUrl.getAdherentsByParams(params).then(
+							function(adherents) {
+								$scope.adherents = adherents;
+							});
+				}
+
+				$scope.addSize = function() {
+					$scope.sizeArrayAd += 20;
+					if ($scope.sizeArrayAd >= $scope.adherents.length) {
+						$scope.sizeMaxAd = true;
+					}
+				}
+
+			})
+
 })();
--- src/main/webapp/media/controllers/rechercheMediaController.js
+++ src/main/webapp/media/controllers/rechercheMediaController.js
@@ -1,92 +1,115 @@
-(function(){
+(function() {
 	'use strict';
-	
-	angular.module('mediaticApp.media')
-	.config(function($routeProvider){
-		$routeProvider.when('/medias',{
-			templateUrl:'/media/templates/rechercheMedia.html',
-			controller:'RechercheMediaCtrl'
+
+	angular.module('mediaticApp.media').config(function($routeProvider) {
+		$routeProvider.when('/medias', {
+			templateUrl : '/media/templates/rechercheMedia.html',
+			controller : 'RechercheMediaCtrl'
 		})
-	})
-	.controller('RechercheMediaCtrl',function($scope, $routeParams, $rootScope, ServiceUrl){
-		$rootScope.title = "Recherche Médias";
-		
-		$scope.sizeArrayMedia = 10;
-		$scope.sizeMaxMedia = false;
-		
-		$scope.medias = [];
-		ServiceUrl.getMedias().then(function(medias){
-			$scope.medias = medias;
-		});
-		
-		$scope.sortedId = false;
-		
-		$scope.sortId = function() {
-			$scope.sortedId = !$scope.sortedId;
-			var params = {tri: "id"};
-			if($scope.sortedId)
-			{
-				params.tri = "-id";
-			}
-			ServiceUrl.getMediasByParams(params).then(function(medias){
-				$scope.medias = medias;
-			});
-		}
-		
-		$scope.sortedTitre = false;
-		
-		$scope.sortTitre = function() {
-			$scope.sortedTitre = !$scope.sortedTitre;
-			var params = {tri: "titre"};
-			if($scope.sortedTitre)
-			{
-				params.tri = "-titre";
-			}
-			ServiceUrl.getMediasByParams(params).then(function(medias){
-				$scope.medias = medias;
-			});
-		}
-		
-		$scope.sortedAuteur = false;
-		
-		$scope.sortAuteur = function() {
-			$scope.sortedAuteur = !$scope.sortedAuteur;
-			var params = {tri: "auteur"};
-			if($scope.sortedAuteur)
-			{
-				params.tri = "-auteur";
-			}
-			ServiceUrl.getMediasByParams(params).then(function(medias){
-				$scope.medias = medias;
-			});
-		}
-		
-		$scope.startSearch = function() {
-			var params = {};
-			if($scope.search.titre != undefined)
-			{
-				params.titre = $scope.search.titre;
-			}
-			if($scope.search.auteur != undefined)
-			{
-				params.auteur = $scope.search.auteur;
-			}
-			if($scope.search.type != undefined)
-			{
-				params.type = $scope.search.type;
-			}
-			ServiceUrl.getMediasByParams(params).then(function(medias){
-				$scope.medias = medias;
-			});
-		}
-		
-		$scope.addSize = function(){
-			$scope.sizeArrayMedia += 5;
-			if($scope.sizeArrayMedia >= $scope.medias.length){
-				$scope.sizeMaxMedia = true;
-			}
-		}
-		
-	})
-	
+	}).controller('RechercheMediaCtrl',
+			function($scope, $routeParams, $rootScope, ServiceUrl) {
+				$rootScope.title = "Recherche Médias";
+
+				$scope.sizeArrayMedia = 10;
+				$scope.sizeMaxMedia = false;
+
+				$scope.medias = [];
+				ServiceUrl.getMedias().then(function(medias) {
+					$scope.medias = medias;
+				});
+
+				$scope.resetSort = function(sortValue) {
+					if (sortValue != $scope.sortedIdAsc)
+						$scope.sortedIdAsc = false;
+					if (sortValue != $scope.sortedIdDesc)
+						$scope.sortedIdDesc = false;
+					if (sortValue != $scope.sortedTitreAsc)
+						$scope.sortedTitreAsc = false;
+					if (sortValue != $scope.sortedTitreDesc)
+						$scope.sortedTitreDesc = false;
+					if (sortValue != $scope.sortedAuteurAsc)
+						$scope.sortedAuteurAsc = false;
+					if (sortValue != $scope.sortedAuteurDesc)
+						$scope.sortedAuteurDesc = false;
+					if (sortValue != $scope.sortedDisponibiliteAsc)
+						$scope.sortedDisponibiliteAsc = false;
+					if (sortValue != $scope.sortedDisponibiliteDesc)
+						$scope.sortedDisponibiliteDesc = false;
+				}
+
+				$scope.resetSort();
+
+				$scope.sortId = function() {
+					if ($scope.sortedIdAsc) {
+						$scope.resetSort($scope.sortedIdAsc);
+						$scope.sortedIdAsc = false;
+						$scope.sortedIdDesc = true;
+					} else {
+						$scope.resetSort($scope.sortedIdDesc);
+						$scope.sortedIdDesc = false;
+						$scope.sortedIdAsc = true;
+					}
+				}
+
+				$scope.sortTitre = function() {
+					if ($scope.sortedTitreAsc) {
+						$scope.resetSort($scope.sortedTitreAsc);
+						$scope.sortedTitreAsc = false;
+						$scope.sortedTitreDesc = true;
+					} else {
+						$scope.resetSort($scope.sortedTitreDesc);
+						$scope.sortedTitreDesc = false;
+						$scope.sortedTitreAsc = true;
+					}
+				}
+
+				$scope.sortAuteur = function() {
+					if ($scope.sortedAuteurAsc) {
+						$scope.resetSort($scope.sortedAuteurAsc);
+						$scope.sortedAuteurAsc = false;
+						$scope.sortedAuteurDesc = true;
+					} else {
+						$scope.resetSort($scope.sortedAuteurDesc);
+						$scope.sortedAuteurDesc = false;
+						$scope.sortedAuteurAsc = true;
+					}
+				}
+
+				$scope.sortDisponibilite = function() {
+					if ($scope.sortedDisponibiliteAsc) {
+						$scope.resetSort($scope.sortedDisponibiliteAsc);
+						$scope.sortedDisponibiliteAsc = false;
+						$scope.sortedDisponibiliteDesc = true;
+					} else {
+						$scope.resetSort($scope.sortedDisponibiliteDesc);
+						$scope.sortedDisponibiliteDesc = false;
+						$scope.sortedDisponibiliteAsc = true;
+					}
+				}
+				
+				$scope.startSearch = function() {
+					var params = {};
+					if ($scope.search.titre != undefined) {
+						params.titre = $scope.search.titre;
+					}
+					if ($scope.search.auteur != undefined) {
+						params.auteur = $scope.search.auteur;
+					}
+					if ($scope.search.type != undefined) {
+						params.type = $scope.search.type;
+					}
+					ServiceUrl.getMediasByParams(params).then(function(medias) {
+						$scope.medias = medias;
+					});
+				}
+
+				$scope.addSize = function() {
+					$scope.sizeArrayMedia += 5;
+					if ($scope.sizeArrayMedia >= $scope.medias.length) {
+						$scope.sizeMaxMedia = true;
+					}
+				}
+
+			})
+
 })();
